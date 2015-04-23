c77e909740350674bee8d1fee64f5eb2dac01fe7
Ryan Glassett <rglassett@gmail.com>
2015-04-23 10:51:43 -0700

    Update dotfiles

--- rails/template.rb
+++ rails/template.rb
@@ -1,12 +1,7 @@
-gem 'bootstrap-sass'
-
 gem_group :development, :test do
-  gem 'annotate'
   gem 'better_errors'
   gem 'binding_of_caller'
   gem 'pry-rails'
-  gem 'rspec'
-  gem 'rspec-rails'
 end
 
 run "bundle install"
