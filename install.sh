4a5bf17d5b2a7a96553ddea88a115d1ce70b23e1
Ryan Glassett <rglassett@gmail.com>
2016-01-07 17:44:44 -0800

    Use Plug to manage vim plugins

--- install.sh
+++ install.sh
@@ -67,7 +67,8 @@ fi
 open ~/.iTerm2/Solarized\ Dark\ Deutan.itermcolors
 open ~/.iTerm2/Solarized\ Light\ Deutan.itermcolors
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim "+:PluginInstall" "+:qa"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim "+:PlugInstall" "+:qa"
 
 source $HOME/.osx
