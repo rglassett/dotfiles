53b9f82d8538bcb0bf2b2beec2afa362eae33bef
Ryan Glassett <rglassett@gmail.com>
2016-04-03 11:39:08 -0700

    Use homebrew-bundle to install brews and casks

---
+++ Brewfile
@@ -0,0 +1,34 @@
+tap 'caskroom/cask'
+tap 'homebrew/bundle'
+tap 'thoughtbot/formulae'
+
+# Unix
+brew 'autojump'
+brew 'ctags'
+brew 'git'
+brew 'heroku-toolbelt'
+brew 'imagemagick'
+brew 'node'
+brew 'openssl'
+brew 'qt'
+brew 'rbenv'
+brew 'readline'
+brew 'reattach-to-user-namespace'
+brew 'ruby-build'
+brew 'the_silver_searcher'
+brew 'thoughtbot/formulae/rcm'
+brew 'tmux'
+brew 'vim'
+brew 'zsh'
+
+# Databases
+brew 'postgresql', restart_service: true
+brew 'redis', restart_service: true
+
+# OS X
+cask 'dropbox'
+cask 'flux'
+cask 'google-chrome'
+cask 'iterm2'
+cask 'slack'
+cask 'spectacle'
--- install.sh
+++ install.sh
@@ -1,43 +1,12 @@
 #!/bin/sh
 
-# TODO: Write functions to do this stuff conditionally
-
 RUBY_VERSION=2.2.2
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew update
 
-brew install zsh
+brew bundle --file=$HOME/dotfiles/Brewfile
+
 chsh -s $(which zsh)
-
-brew install autojump
-brew install ctags
-brew install git
-brew install heroku-toolbelt
-brew install imagemagick
-brew install node
-brew install phantomjs
-brew install qt
-brew install rbenv
-brew install readline
-brew install reattach-to-user-namespace
-brew install redis
-brew install ruby-build
-brew install sqlite
-brew install the_silver_searcher
-brew install tmux
-brew install vim
-
-brew install openssl
-brew unlink openssl && brew link openssl --force
-
-brew install postgresql
-ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
-
-brew install redis
-ln -sfv /usr/local/opt/redis/*.plist ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.redis.plist
 
 rbenv install "$RUBY_VERSION"
 rbenv global "$RUBY_VERSION"
@@ -46,16 +15,8 @@ rbenv shell "$RUBY_VERSION"
 gem install bundler
 rbenv rehash
 
-brew cask install dropbox
-brew cask install flux
-brew cask install google-chrome
-brew cask install iterm2
-brew cask install slack
-
-brew tap thoughtbot/formulae
-brew install rcm
-
-brew cleanup
+number_of_cores=$(sysctl -n hw.ncpu)
+bundle config --global jobs $((number_of_cores - 1))
 
 if [[ ! -d "$HOME/dotfiles" ]]; then
   git clone git@github.com:rglassett/dotfiles.git $HOME/dotfiles
