ffb1e39d3cd778d76bdbdcba3a4eb1a1e693a824
Ryan Glassett <rglassett@gmail.com>
2016-04-03 11:58:22 -0700

    Tweak install script
    - Use latest Ruby version
    - Ensure PATH is correct when necessary
    - Add some descriptive comments

--- install.sh
+++ install.sh
@@ -1,31 +1,41 @@
 #!/bin/sh
 
-RUBY_VERSION=2.2.2
-
+# Homebrew installation
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
+export PATH="/usr/local/bin:$PATH"
 brew bundle --file=$HOME/dotfiles/Brewfile
 
+# Use zshell
 chsh -s $(which zsh)
 
-rbenv install "$RUBY_VERSION"
-rbenv global "$RUBY_VERSION"
-rbenv shell "$RUBY_VERSION"
+# Ruby installation
+find_latest_ruby() {
+  rbenv install --list | grep --invert-match - | tail -1 | sed -e 's/^ *//'
+}
 
+ruby_version="$(find_latest_ruby)"
+rbenv install "$ruby_version"
+rbenv global "$ruby_version"
+rbenv shell "$ruby_version"
+
+# Bundler config
 gem install bundler
 rbenv rehash
-
 number_of_cores=$(sysctl -n hw.ncpu)
 bundle config --global jobs $((number_of_cores - 1))
 
+# Symlink dotfiles
 if [[ ! -d "$HOME/dotfiles" ]]; then
   git clone git@github.com:rglassett/dotfiles.git $HOME/dotfiles
   env RCRC=$HOME/dotfiles/rcrc rcup
 fi
 
-open ~/.iTerm2/Solarized\ Dark\ Deutan.itermcolors
-open ~/.iTerm2/Solarized\ Light\ Deutan.itermcolors
+# Add iTerm colorschemes
+open $HOME/.iTerm2/Solarized\ Dark\ Deutan.itermcolors
+open $HOME/.iTerm2/Solarized\ Light\ Deutan.itermcolors
 
+# Install vim plugins
 vim "+:PlugInstall" "+:qa"
 
+# OS X configuration
 source $HOME/.osx
