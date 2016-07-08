f984c9320933ceb77656f3c91bac4768175e75a2
Ryan Glassett <rglassett@gmail.com>
2016-07-08 10:37:16 -0700

    Use nvm to install and manage node versions

--- install.sh
+++ install.sh
@@ -33,6 +33,19 @@ rbenv rehash
 number_of_cores=$(sysctl -n hw.ncpu)
 bundle config --global jobs $((number_of_cores - 1))
 
+# Node installation
+find_latest_node() {
+  nvm ls-remote | grep --invert-match - | tail -1 | sed -e 's/^ *//'
+}
+
+mkdir -p $HOME/.nvm
+source "$(brew --prefix nvm)/nvm.sh"
+
+node_version="$(find_latest_node)"
+nvm install $node_version
+nvm use $node_version
+nvm alias default $node_version
+
 # Add iTerm colorschemes
 open $HOME/.iTerm2/Solarized\ Dark\ Deutan.itermcolors
 open $HOME/.iTerm2/Solarized\ Light\ Deutan.itermcolors
