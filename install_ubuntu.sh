ce03fd344421a6547b7e737a4e644dcbb62e9a0f
Ryan Glassett <rglassett@gmail.com>
2019-02-20 21:07:47 -0800

    Add Ubuntu installation script

---
+++ install_ubuntu.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install git
+sudo apt-get install i3
+sudo apt-get install rbenv
+sudo apt-get install ruby-build
+sudo apt-get install stow
+sudo apt-get install xcape
+sudo apt-get install xclip
+sudo apt-get install zsh
+chsh -s /usr/bin/zsh
